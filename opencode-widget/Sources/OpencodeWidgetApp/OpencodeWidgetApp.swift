bfa4ed409c5d5850330ac4c37db1f8010a86a9ee
Fathurrahman Lananan <fathur6@gmail.com>
2026-06-30 21:12:56 +0800

    task-9: add SwiftUI MenuBarExtra app entry point + LaunchAgent plist

--- opencode-widget/Sources/OpencodeWidgetApp/OpencodeWidgetApp.swift
+++ opencode-widget/Sources/OpencodeWidgetApp/OpencodeWidgetApp.swift
@@ -1,9 +1,31 @@
-import AppKit
+import SwiftUI
+import OpencodeWidgetShared
 
 @main
-struct OpencodeWidgetApp {
-    static func main() {
-        let app = NSApplication.shared
-        app.run()
+struct OpencodeWidgetApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        MenuBarExtra("Opencode Widget", systemImage: "chart.bar.fill") {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var refreshTimer: Timer?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        Task {
+            let cache = await DataFetcher.refreshAll()
+            DataStore.save(cache: cache)
+        }
+
+        refreshTimer = Timer.scheduledTimer(withTimeInterval: 900, repeats: true) { _ in
+            Task {
+                let cache = await DataFetcher.refreshAll()
+                DataStore.save(cache: cache)
+            }
+        }
     }
 }
